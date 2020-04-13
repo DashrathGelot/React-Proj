c09e45befe77c4febf2c10bc8ece7a6c7324b6dd
DashrathGelot <gelotdashrath9731@gmail.com>
2020-04-13 08:31:16 +0530

    student-crud-app with redux

---
+++ student-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Student from './component/Student';
+
+function App() {
+  return (
+    <div>
+        <Student />
+    </div>
+  );
+}
+
+export default App;
---
+++ student-app/src/reducer/AppReducer.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import StudentReducer from './StudentReducer';
+import FormReducer from './FormReducer'
+
+export default combineReducers({
+    studentStore:StudentReducer,
+    studentForm:FormReducer
+})
