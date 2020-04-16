35c1dda173e5fdf8874d2d8f84e22770d998409f
DashrathGelot <gelotdashrath9731@gmail.com>
2020-04-16 14:41:08 +0530

    contact diary app with async action

---
+++ contact-diary/src/services/ContactService.js
@@ -0,0 +1,17 @@
+import {AjaxCallBegin,AjaxCallSuccess,AjaxCallFailed} from '../actions/ContactAction'
+
+export default function(dispatch){
+    dispatch(AjaxCallBegin())
+    fetch("https://jsonplaceholder.typicode.com/users",{
+    })
+    .then(resp=>{
+        if(!resp.ok) throw Error(resp.statusText)
+       return resp.json();
+    })
+    .then(json=>{
+        dispatch(AjaxCallSuccess(json))
+    })
+    .catch(err=>{
+        dispatch(AjaxCallFailed(err))
+    })
+}
